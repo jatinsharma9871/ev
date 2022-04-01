74fbd2274c495a11504d0d65f248e8422183340e
jatin sharma <jatinsharma9871@gmail.com>
2022-04-01 16:43:36 +0000

    a

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 console.log(API_URL);
 
 function renderDynamicContent() {
-    document.getElementById('dynamic-content').innerHTML = "Dynamic content"
+   let r =  document.getElementById('dynamic-content').innerHTML = "Dynamic content"
+    var box1 = document.getElementById("Text1")
+    var box2 = document.getElementById("Text2")
+    var box3 = document.getElementById("txtresult")
+   
+    let count = box1+box2
+    r.append(count)
+
 }
 
